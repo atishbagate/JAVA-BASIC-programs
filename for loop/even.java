70812e27a3a3d467f3c8828e3721c462d7e2ddd5
atishbagate <40466303+atishbagate@users.noreply.github.com>
2020-04-23 08:57:59 +0530

    for loop
    example

---
+++ for loop/even.java
@@ -0,0 +1,17 @@
+class even // pl the even nos bewn 1 to 10
+{
+	public static void main(String[] args)
+	{ 
+		int i = 1;
+		for (i = 1;i<=10 ;i+=1 ) {
+			if (i%2 ==0)
+			{
+				System.out.println(i);
+	 	        System.out.println("even nos");
+				
+			}
+			
+			
+		}
+	}
+}
