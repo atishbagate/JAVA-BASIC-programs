0e0164c9908453d8f6299b7310806addf001e8de
atishbagate <40466303+atishbagate@users.noreply.github.com>
2020-04-27 08:33:01 +0530

    Add files via upload

---
+++ 3 data transfer/odd.java
@@ -0,0 +1,28 @@
+/*write a function to find the area of fthe rectangle
+*/
+
+
+
+class odd
+
+
+
+{
+	public static void main(String[] args) 
+	{
+		tran();
+	}
+
+public static void tran()
+{
+	int len = 4;
+	int breat = 4;
+
+	int area;
+	System.out.println("area of trangle: ");
+	System.out.println(area = len * breat);
+    
+}
+
+
+}
