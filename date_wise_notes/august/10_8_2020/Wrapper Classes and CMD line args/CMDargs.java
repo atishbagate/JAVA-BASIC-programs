e172403b8b9a6bcffb3d63074d6f3381a4904c27
atishbagate <40466303+atishbagate@users.noreply.github.com>
2020-08-15 15:35:17 +0530

    Add files via upload

---
+++ date_wise_notes/august/10_8_2020/Wrapper Classes and CMD line args/CMDargs.java
@@ -0,0 +1,14 @@
+class A{
+	
+	public static void main(String[] args) {
+		System.out.println("main starts...");
+
+		for (int i = 0 ;i<args.length ;i++ ) {
+			System.out.println(args[i]);
+		}
+
+
+		System.out.println("main  ends...");
+		return;
+	}
+}
